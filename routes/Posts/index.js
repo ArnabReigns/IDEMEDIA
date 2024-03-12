0329ae85f0c219add9fa079ec35e41db55d849f7
Arnab Chatterjee <arnabchatterjee912@gmail.com>
2024-03-13 00:28:59 +0530

    gf

--- routes/Posts/index.js
+++ routes/Posts/index.js
@@ -4,7 +4,14 @@ const required = require("../../utils/required");
 const internalError = require("../../utils/InternalError");
 const router = Router();
 
-router.get("/get-all", async (req, res) => {});
+router.get("/get-all", async (req, res) => {
+  try {
+    const posts = await Posts.find({});
+    return res.json({ posts });
+  } catch (e) {
+    internalError(res);
+  }
+});
 
 router.post("/create-new", required(["username"]), async (req, res) => {
   const { caption, img, username } = req.body;
