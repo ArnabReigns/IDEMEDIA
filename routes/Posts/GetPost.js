defdf41f1486d1fd8940505d1bb45b2d9d6d2421
Arnab Chatterjee <arnabchatterjee912@gmail.com>
2024-03-21 01:22:52 +0530

    gf

--- routes/Posts/GetPost.js
+++ routes/Posts/GetPost.js
@@ -1,8 +1,10 @@
+const Comment = require("../../models/CommentModel");
 const Posts = require("../../models/postModel");
 
 const GetPost = async (req, res) => {
   const post_id = req.query.id;
   console.log(post_id);
+
   try {
     const post = await Posts.findById(post_id)
       .populate("likes", "-posts -followers -following -liked_posts -password")
@@ -25,7 +27,9 @@ const GetPost = async (req, res) => {
         ],
       })
       .exec();
-    return res.json(post);
+    console.log(post);
+    const comments = await Comment.find({ post_id: post_id });
+    return res.json({ post, total_comment: comments.length });
   } catch (e) {
     console.log(e);
     return internalError(res);
